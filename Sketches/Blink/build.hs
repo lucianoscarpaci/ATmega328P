14e1a98f036706a79b0125b1b3caa41bf38439e1
Luciano Scarpaci <16402942+lucianoscarpaci@users.noreply.github.com>
2025-03-05 01:34:36 -0500

    Automate Compiling, Flashing, and Serial
    +Automation with compiling
    -Serial Reading is next to solve

---
+++ Sketches/Blink/build.hs
@@ -0,0 +1,47 @@
+#!/usr/bin/env runhaskell
+import System.Process (system)
+import System.Exit (ExitCode(..))
+import Control.Concurrent (threadDelay)
+
+-- Arduino parameters
+boardFQBN :: String
+boardFQBN = "arduino:avr:nano:cpu=atmega328old"
+
+port :: String
+port = "/dev/cu.usbserial-1420"
+
+sketchPath :: String
+sketchPath = "$PWD/build"
+
+baudRate :: String
+baudRate = "9600"
+
+targetFile :: String
+targetFile = "$PWD/build/build.ino"
+
+-- Helper to run a shell command
+runCommand :: String -> IO ()
+runCommand cmd = do
+    putStrLn $ "Running: " ++ cmd
+    exitCode <- system cmd
+    case exitCode of
+        ExitSuccess -> putStrLn "Success!"
+        ExitFailure code -> putStrLn $ "Failed with code: " ++ show code
+
+-- Main function
+main :: IO ()
+main = do
+    -- Compile the sketch
+    runCommand $ "arduino-cli compile --fqbn " ++ boardFQBN ++ " --output-dir " ++ sketchPath ++ " " ++ targetFile
+
+    -- Add a delay to allow the serial port to be ready
+    threadDelay (2 * 1000000)
+
+    -- Upload the sketch
+    runCommand $ "arduino-cli upload -p " ++ port ++ " -b " ++ boardFQBN ++ " --input-dir " ++ sketchPath
+
+    -- Add a delay to allow the serial port to be ready
+    threadDelay (2 * 1000000)
+
+    -- Open the serial monitor
+    runCommand $ "screen " ++ port ++ " " ++ baudRate
--- shell.nix
+++ shell.nix
@@ -6,5 +6,12 @@ mkShell {
     haskellPackages.ghc
     haskellPackages.cabal-install
     arduino-cli
+    pkgs.haskellPackages.stack
+    pkgs.haskellPackages.haskell-language-server
   ];
+
+  # Optional: Set environment variables
+  shellHook = ''
+    export PATH=$PATH:$HOME/.local/bin
+  '';
 }
