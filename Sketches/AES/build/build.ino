6a3170e6c90b9b6a9c821a380a05630c06f162c9
Luciano Scarpaci <16402942+lucianoscarpaci@users.noreply.github.com>
2025-03-07 15:54:07 -0500

    Update build.ino

--- Sketches/AES/build/build.ino
+++ Sketches/AES/build/build.ino
@@ -1,15 +1,13 @@
-/* Simple buffer example for low-memory conditions (Arduino) */
-
 #include "AESLib.h"
 #include <EEPROM.h>
 #define BAUD 9600
 
 AESLib aesLib;
 
-#define INPUT_BUFFER_LIMIT (128 + 1) // designed for Arduino UNO, not stress-tested anymore (this works with readBuffer[129])
+#define INPUT_BUFFER_LIMIT (128 + 1) 
 
-unsigned char cleartext[INPUT_BUFFER_LIMIT] = {0}; // THIS IS INPUT BUFFER (FOR TEXT)
-unsigned char ciphertext[2*INPUT_BUFFER_LIMIT] = {0}; // THIS IS OUTPUT BUFFER (FOR BASE64-ENCODED ENCRYPTED DATA)
+unsigned char plaintext[INPUT_BUFFER_LIMIT] = {0}; // THIS IS INPUT BUFFER (FOR TEXT)
+unsigned char ciphertext[2*INPUT_BUFFER_LIMIT] = {0}; // THIS IS OUTPUT BUFFER (FOR ENCRYPTED DATA)
 
 unsigned char readBuffer[16] = "Encrypt It!xxxx";
 
@@ -23,7 +21,6 @@ void generate_aes_key() {
   }
 }
 
-// General initialization vector (same as in node-js example) (you must use your own IV's in production for full security!!!)
 byte aes_iv[N_BLOCK];
 
 // Generate IV (once)
@@ -39,9 +36,9 @@ uint16_t encrypt_to_ciphertext(char * msg, uint16_t msgLen, byte iv[]) {
   return cipherlength;
 }
 
-uint16_t decrypt_to_cleartext(byte msg[], uint16_t msgLen, byte iv[]) {
+uint16_t decrypt_to_plaintext(byte msg[], uint16_t msgLen, byte iv[]) {
   Serial.print("Calling decrypt...; ");
-  uint16_t dec_bytes = aesLib.decrypt(msg, msgLen, (byte*)cleartext, aes_key, sizeof(aes_key), iv);
+  uint16_t dec_bytes = aesLib.decrypt(msg, msgLen, (byte*)plaintext, aes_key, sizeof(aes_key), iv);
   Serial.print("Decrypted bytes: "); Serial.println(dec_bytes);
   return dec_bytes;
 }
@@ -65,11 +62,7 @@ void wait(unsigned long milliseconds) {
 
 unsigned long loopcount = 0;
 
-// Working IV buffer: Will be updated after encryption to follow up on next block.
-// But we don't want/need that in this test, so we'll copy this over with enc_iv_to/enc_iv_from
-// in each loop to keep the test at IV iteration 1. We could go further, but we'll get back to that later when needed.
-
-// General initialization vector (same as in node-js example) (you must use your own IV's in production for full security!!!)
+// AES IV for each encryption
 byte enc_iv[N_BLOCK];
 byte enc_iv_to[N_BLOCK];
 byte enc_iv_from[N_BLOCK];
@@ -79,24 +72,24 @@ void loop() {
   Serial.print("readBuffer length: "); Serial.println(sizeof(readBuffer));
 
    // must not exceed INPUT_BUFFER_LIMIT bytes; may contain a newline
-  sprintf((char*)cleartext, "%s", readBuffer);
+  sprintf((char*)plaintext, "%s", readBuffer);
 
   // Encrypt
   // iv_block gets written to, provide own fresh copy... so each iteration of encryption will be the same.
   uint16_t msgLen = sizeof(readBuffer);
   aesLib.gen_iv(enc_iv); // Generate a new random IV for each encryption
   memcpy(enc_iv_to, enc_iv, sizeof(enc_iv));
-  uint16_t encLen = encrypt_to_ciphertext((char*)cleartext, msgLen, enc_iv);
+  uint16_t encLen = encrypt_to_ciphertext((char*)plaintext, msgLen, enc_iv);
   Serial.print("Encrypted length = "); Serial.println(encLen );
 
   Serial.println("Encrypted. Decrypting..."); Serial.println(encLen ); Serial.flush();
   
   memcpy(enc_iv_from, enc_iv_to, sizeof(enc_iv_to));
-  uint16_t decLen = decrypt_to_cleartext(ciphertext, encLen, enc_iv_from);
-  Serial.print("Decrypted cleartext of length: "); Serial.println(decLen);
-  Serial.print("Decrypted cleartext:\n"); Serial.println((char*)cleartext);
+  uint16_t decLen = decrypt_to_plaintext(ciphertext, encLen, enc_iv_from);
+  Serial.print("Decrypted plaintext of length: "); Serial.println(decLen);
+  Serial.print("Decrypted plaintext:\n"); Serial.println((char*)plaintext);
 
-  if (strcmp((char*)readBuffer, (char*)cleartext) == 0) {
+  if (strcmp((char*)readBuffer, (char*)plaintext) == 0) {
     Serial.println("Decrypted correctly.");
   } else {
     Serial.println("Decryption test failed.");
