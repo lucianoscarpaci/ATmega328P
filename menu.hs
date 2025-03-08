9d3a4993cb4f08314048f9be18dc53afead4091a
Luciano Scarpaci <16402942+lucianoscarpaci@users.noreply.github.com>
2025-03-08 11:45:02 -0500

    Update menu.hs

--- menu.hs
+++ menu.hs
@@ -11,15 +11,19 @@ menu :: InputT IO ()
 menu = do
     outputStrLn "Select a sketch:"
     outputStrLn "1. AES"
-    outputStrLn "2. EEA"
-    outputStrLn "3. Exit"
-    outputStrLn "4. Open Serial Monitor"
+    outputStrLn "2. Blink"
+    outputStrLn "3. EEA"
+    outputStrLn "4. RNG"
+    outputStrLn "5. Exit"
+    outputStrLn "6. Open Serial Monitor"
     choice <- getInputLine "Enter choice: "
     case choice of
         Just "1" -> liftIO (runCommand "cd ./Sketches/AES && ./flash") >> menu
-        Just "2" -> liftIO (runCommand "cd ./Sketches/EEA && ./flash") >> menu
-        Just "3" -> outputStrLn "Exiting..."
-        Just "4" -> liftIO (runCommand "screen /dev/cu.usbserial-1420 9600")
+        Just "2" -> liftIO (runCommand "cd ./Sketches/Blink && ./flash") >> menu
+        Just "3" -> liftIO (runCommand "cd ./Sketches/EEA && ./flash") >> menu
+        Just "4" -> liftIO (runCommand "cd ./Sketches/RNG && ./flash") >> menu 
+        Just "5" -> outputStrLn "Exiting..."
+        Just "6" -> liftIO (runCommand "screen /dev/cu.usbserial-1420 9600")
         _        -> outputStrLn "Invalid choice, try again." >> menu
 
 -- Helper to run a shell command
